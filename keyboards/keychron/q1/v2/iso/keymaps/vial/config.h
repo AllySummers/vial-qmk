e3bc6dd231decd34870cc7f84e6f7c6953c075be
adophoxia <andyao1528@gmail.com>
2022-12-14 21:36:29 -0800

    Port VIAL to Keychron Q1v2

---
+++ keyboards/keychron/q1/v2/iso/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA1, 0xEE, 0xD0, 0x24, 0x3F, 0x11, 0x6B, 0x2F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
