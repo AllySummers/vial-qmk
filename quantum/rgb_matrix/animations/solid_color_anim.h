3c9bb6ff89a90073775f78a9667e274e62313d2a
Ally Summers <ally.summers@outlook.com.au>
2023-01-13 23:20:02 +1100

    fixing broken qmk

--- quantum/rgb_matrix/animations/solid_color_anim.h
+++ quantum/rgb_matrix/animations/solid_color_anim.h
@@ -2,15 +2,13 @@
 RGB_MATRIX_EFFECT(SOLID_COLOR)
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-bool SOLID_COLOR(effect_params_t* params) {
-    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+static HSV SOLID_COLOR_math(HSV hsv, uint8_t i, uint8_t time) {
+    hsv.h += abs8(g_led_config.point[i].y - k_rgb_matrix_center.y) + (g_led_config.point[i].x - time);
+    return hsv;
+}
 
-    RGB rgb = rgb_matrix_hsv_to_rgb(rgb_matrix_config.hsv);
-    for (uint8_t i = led_min; i < led_max; i++) {
-        RGB_MATRIX_TEST_LED_FLAGS();
-        rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-    }
-    return rgb_matrix_check_finished_leds(led_max);
+bool SOLID_COLOR(effect_params_t* params) {
+    return effect_runner_i(params, &SOLID_COLOR_math);
 }
 
 #endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
