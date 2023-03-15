1fc130be744186c47a8f4424036ead39b911ad98
Ally Summers <asummers@atlassian.com>
2023-03-15 17:02:55 +1100

    update q1 keymap

---
+++ keyboards/keychron/q1/ansi_stm32l432_encoder/keymaps/hewlo/keymap.c
@@ -0,0 +1,155 @@
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "keychron_common.h"
+
+enum layers {
+    BASE,
+    FN_1,
+    FN_2,
+    FN_3
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [BASE] = LAYOUT_ansi_82(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_PSCR,            TD(0) ,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,            KC_INS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS,            KC_DEL,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,             KC_ENT,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(FN_1),  KC_RGUI,  KC_LEFT,  KC_DOWN,  KC_RGHT
+      ),
+
+    [FN_1] = LAYOUT_ansi_82(
+        KC_TRNS,  KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,    KC_F24,   KC_TRNS,            RGB_TOG,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,            KC_TRNS,
+        KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_PGUP,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                MO(FN_2), KC_TRNS,   KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END
+      ),
+
+    [FN_2] = LAYOUT_ansi_82(
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,            KC_TRNS,
+        KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,   MO(FN_3),  KC_TRNS,  KC_TRNS,  KC_TRNS
+      ),
+
+    [FN_3] = LAYOUT_ansi_82(
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            QK_BOOT,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            DB_TOGG,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,            EE_CLR ,
+        KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+      ),
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [BASE] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [FN_1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [FN_2] = { ENCODER_CCW_CW( KC_NO ,  KC_NO ) },
+    [FN_3] = { ENCODER_CCW_CW( KC_NO ,  KC_NO ) },
+};
+#endif // ENCODER_MAP_ENABLE
+
+void keyboard_post_init_user(void) {
+    vial_tap_dance_entry_t tap_dance_0 = {
+        KC_MPLY,
+        MO(1),
+        KC_MUTE,
+        KC_NO,
+        200
+    };
+
+    dynamic_keymap_set_tap_dance(0, &tap_dance_0);
+}
+
+void rgb_matrix_indicators_user(void) {
+    uint8_t layer = get_highest_layer(layer_state);
+	  uint8_t modifiers = get_mods();
+
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(45, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_LEFT_SHIFT)) {
+        rgb_matrix_set_color(59, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_LEFT_CTRL)) {
+        rgb_matrix_set_color(72, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_LEFT_GUI)) {
+        rgb_matrix_set_color(73, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_LEFT_ALT)) {
+        rgb_matrix_set_color(74, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_RIGHT_ALT)) {
+        rgb_matrix_set_color(76, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_RIGHT_GUI)) {
+        rgb_matrix_set_color(78, RGB_RED);
+    }
+
+    if (modifiers & MOD_BIT(KC_RIGHT_SHIFT)) {
+        rgb_matrix_set_color(80, RGB_RED);
+    }
+
+    switch (layer) {
+        case BASE:
+            rgb_matrix_set_color(1, RGB_RED);
+            break;
+        case FN_1:
+            rgb_matrix_set_color(2, RGB_RED);
+            break;
+        case FN_2:
+            rgb_matrix_set_color(3, RGB_RED);
+            break;
+        case FN_3:
+            rgb_matrix_set_color(4, RGB_RED);
+            break;
+        default:
+            break;
+    }
+}
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    layer_invert(FN_3);
+
+    return true;
+}
+
+void housekeeping_task_user(void) {
+    housekeeping_task_keychron();
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_keychron(keycode, record)) {
+        return false;
+    }
+    return true;
+}
