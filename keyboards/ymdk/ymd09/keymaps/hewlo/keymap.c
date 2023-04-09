735665d025707224bc424469801dd0d6684a62ff
Ally Summers <ally.summers@outlook.com.au>
2023-04-09 21:38:06 +1000

    update ymdk keymap

--- keyboards/ymdk/ymd09/keymaps/hewlo/keymap.c
+++ keyboards/ymdk/ymd09/keymaps/hewlo/keymap.c
@@ -29,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [FN_3] = LAYOUT(
-    KC_TRNS,    KC_TRNS,    KC_TRNS,
-    KC_TRNS,    KC_TRNS,    KC_TRNS,
-    KC_TRNS,    KC_TRNS,    KC_TRNS
+    KC_TRNS,    KC_TRNS,    EE_CLR,
+    KC_TRNS,    KC_TRNS,    DB_TOGG,
+    KC_TRNS,    KC_TRNS,    QK_BOOT
   ),
 };
 
@@ -44,80 +44,65 @@ enum combo_events {
   GOTO_FN_2,
   GOTO_FN_3,
   /* END - do not move index */
-  LAYER_UP,
-  LAYER_DOWN,
 };
 
-const uint16_t PROGMEM layer_base_combo[]   = { KC_F20, KC_F16, COMBO_END};
-const uint16_t PROGMEM layer_fn2_combo[]    = { KC_F20, KC_F18, COMBO_END};
-const uint16_t PROGMEM layer_fn1_combo[]    = { KC_F20, KC_F22, COMBO_END};
-const uint16_t PROGMEM layer_fn3_combo[]    = { KC_F20, KC_F24, COMBO_END};
-const uint16_t PROGMEM layer_up_combo[]     = { KC_F20, KC_F17, COMBO_END};
-const uint16_t PROGMEM layer_down_combo[]   = { KC_F20, KC_F23, COMBO_END};
+const uint16_t PROGMEM layer_base_combo[]   = { KC_F20, KC_F21, COMBO_END};
+const uint16_t PROGMEM layer_fn1_combo[]    = { KC_F20, KC_F16, COMBO_END};
+const uint16_t PROGMEM layer_fn2_combo[]    = { KC_F20, KC_F19, COMBO_END};
+const uint16_t PROGMEM layer_fn3_combo[]    = { KC_F20, KC_F22, COMBO_END};
 
-combo_t key_combos[] = {
+combo_t key_combos[VIAL_COMBO_ENTRIES] = {
   [GOTO_BASE]     = COMBO_ACTION(layer_base_combo),
   [GOTO_FN_1]     = COMBO_ACTION(layer_fn2_combo),
   [GOTO_FN_2]     = COMBO_ACTION(layer_fn1_combo),
   [GOTO_FN_3]     = COMBO_ACTION(layer_fn3_combo),
-  [LAYER_UP]      = COMBO_ACTION(layer_up_combo),
-  [LAYER_DOWN]    = COMBO_ACTION(layer_down_combo),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
   if (pressed) {
     layer_clear();
+    layer_off(BASE);
+    layer_off(FN_1);
+    layer_off(FN_2);
+    layer_off(FN_3);
 
     switch(combo_index) {
-      case LAYER_UP:
-        if (layer_state_is(BASE)) {
-          layer_on(FN_1);
-        } else if (layer_state_is(FN_1)) {
-          layer_on(FN_2);
-        } else if (layer_state_is(FN_2)) {
-          layer_on(FN_3);
-        } else if (layer_state_is(FN_3)) {
-          layer_on(BASE);
-        }
-        break;
-      case LAYER_DOWN:
-        if (layer_state_is(BASE)) {
-          layer_on(FN_3);
-        } else if (layer_state_is(FN_1)) {
-          layer_on(BASE);
-        } else if (layer_state_is(FN_2)) {
-          layer_on(FN_1);
-        } else if (layer_state_is(FN_3)) {
-          layer_on(FN_2);
-        }
-        break;
       case GOTO_BASE:
-        layer_on(GOTO_BASE);
+        layer_on(BASE);
         break;
       case GOTO_FN_1:
-        layer_on(GOTO_FN_1);
+        layer_on(FN_1);
         break;
       case GOTO_FN_2:
-        layer_on(GOTO_FN_2);
+        layer_on(FN_2);
         break;
       case GOTO_FN_3:
-        layer_on(GOTO_FN_3);
+        layer_on(FN_3);
         break;
     }
   }
 }
 
 /* === Layer Lighting === */
+
+/*
+  LEDs {
+    5, 0, 1,
+    2, 3, 4,
+    8, 6, 7
+  }
+*/
+
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
     case FN_1:
-      rgblight_setrgb_at(RGB_RED, 1);
+      rgblight_setrgb_at(RGB_RED, 5);
       break;
     case FN_2:
       rgblight_setrgb_at(RGB_RED, 2);
       break;
     case FN_3:
-      rgblight_setrgb_at(RGB_RED, 3);
+      rgblight_setrgb_at(RGB_RED, 8);
       break;
     case BASE:
     default:
